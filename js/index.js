5d77f0fd8089159dd8c051360a8342bf3f1ac327
Manisha Kesarwani <72316941+Manishakesarwani@users.noreply.github.com>
2023-01-28 14:24:34 +0530

    Button Press and key press events in JS added.

---
+++ js/index.js
@@ -0,0 +1,57 @@
+
+var btns = document.querySelectorAll(".drum");
+var w = new Audio("./sounds/tom-1.mp3");
+var a = new Audio("./sounds/tom-2.mp3");
+var s = new Audio("./sounds/tom-3.mp3");
+var d = new Audio("./sounds/tom-4.mp3");
+var j = new Audio("./sounds/snare.mp3");
+var k = new Audio("./sounds/crash.mp3");
+var l = new Audio("./sounds/kick-bass.mp3");
+
+// Button press events...
+for(var i=0; i<btns.length; i++){
+  btns[i].addEventListener("click", function (){
+
+     var InstrumentPlayed = this.innerHTML;
+     makeSound(InstrumentPlayed);
+
+});
+}
+
+// KeyBoard Events...
+document.addEventListener("keypress", function(event){
+  //console.log(a);
+  var keypressed = event.key;
+  makeSound(keypressed);
+
+});
+
+
+// Sounds JS...
+function makeSound(Key){
+  switch (Key) {
+    case "w":
+      w.play();
+      break;
+   case "a":
+       a.play();
+       break;
+   case "s":
+     s.play();
+     break;
+   case "d":
+     d.play();
+     break;
+   case "j":
+     j.play();
+     break;
+   case "k":
+     k.play();
+     break;
+   case "l":
+     l.play();
+     break;
+
+   default: console.log(Key);
+  }
+}
